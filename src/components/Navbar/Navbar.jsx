42edc7a0fe5a71e7a35ed0d6470c4a36f8be5400
shruti2999 <shrutikumari9208@gmail.com>
2025-01-25 20:32:35 +0530

    changes

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='navbar-Left'>
+        <img src={logo}alt="Logo"/>
+        <ul>
+        <li>Home</li>
+        <li>TVShow</li>
+        <li>Movies</li>
+        <li>New & Popular</li>
+        <li>My List</li>
+        <li>Browse By Languages </li>
+        </ul>
+        </div>
+        <div className='navbar-right'>
+          <img src={search_icon} alt="search_icon" className='icons'/>  
+          <p>children</p>
+          <img src ={bell_icon} alt="bell" className='icons'/>
+          <div className="navbar-profile">
+          <img src ={profile_img} alt="" className='profile'/>
+          <img src ={caret_icon} alt="" />
+          <div className='dropdown'>
+          <p>
+            Sign Out Of Netflix
+          </p>
+          </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const TitleCards = ({title,category}) => {
+
+  const [apiData,setapiData]= useState([]); 
+  const cardRef =useRef();
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZmQ0Y2Q4NWMxZTdmY2ExY2MwMjlmNDIxYjk1NGYyOSIsIm5iZiI6MTczNzgwMTIyMS40NzQsInN1YiI6IjY3OTRiZTA1NDRlZjM5Yzg2MDE4ODJlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s8r_Ejym9gubf6zY_5elFtZis33PyDr0Ho4Cw81ZcZM'
+    }
+  };
+  
+  
+
+const handlewheel =(event)=>{
+  event.preventDefault();
+  cardRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(()=>{
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setapiData(res.results))
+    .catch(err => console.error(err));
+    
+  cardRef.current.addEventListener ('wheel',handlewheel);
+  },  []);
+  return (
+    <div className='title- Cards'>
+      <h2>{title?title:"Popular on NetFlix"}</h2>
+      <div className='card-List' ref={cardRef}>
+      {apiData.map((card,index)=>{
+        return <div className='card' key ={index}>
+          <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt=""/>
+          <p>
+            {card.original_title}
+          </p>
+        </div>
+      })}
+      </div>
+    </div>
+  )
+}
+
+export default TitleCards
