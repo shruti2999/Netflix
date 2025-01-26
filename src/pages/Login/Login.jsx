02625b6144df1d77b9ab38e9b982f1fb034d0444
shruti2999 <shrutikumari9208@gmail.com>
2025-01-26 18:44:08 +0530

    changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,7 +8,7 @@ import netflix_spinner from '../../assets/netflix_spinner.gif'
 const Login = () => {
 
     const [signState,setsignState]=useState("Sign In")
-    const [name ,setName]=useState();
+    const [name ,setname]=useState();
     const[email,setemail]=useState();
     const [password,setpassword]=useState();
     const [loading,setloading]=useState(false);
@@ -24,19 +24,19 @@ const Login = () => {
       setloading(false);
     }
   return (
-    loading?<div className="login-spinner">
+    loading?<div className="login_spinner">
       <img src={netflix_spinner}alt=''/>
     </div>:
     <div className='login'>
-      <img src ={logo} className='login-logo' alt=" "/>
-      <div className='login-form'>
+      <img src ={logo} className='login_logo' alt=" "/>
+      <div className='login_form'>
         <h1>{signState}</h1>
-        <form>{signState==="Sign Up"?<input  value={name} onChange={(e)=>{setName(e.target. value)}}type='text' placeholder='UserName'/>:<></>}
+        <form>{signState==="Sign Up"?<input  value={name} onChange={(e)=>{setname(e.target. value)}}type='text' placeholder='UserName'/>:<></>}
           
           <input value={email} onChange={(e)=>{setemail(e.target. value)}} type ='email' placeholder="Email"/>
           <input value={password} onChange={(e)=>{setpassword(e.target. value)}} type ="password" placeholder='password'/>
           <button onClick={user_auth} type='submit'>{signState}</button>
-          <div className='form-help'>
+          <div className='form_help'>
             <div className="remember">
                 <input type="checkbox"/>
                 <label htmlFor="">Remember Me</label>  
@@ -44,7 +44,7 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
-        <div className='form-switch'>
+        <div className='form_switch'>
           {signState==="Sign In"?<p>New to Netflix? <span onClick={()=>{setsignState("Sign Up")}}>
             Sign Up Now</span></p>
           :<p>Already have account?<span onClick={()=>{setsignState("Sign In")}}
